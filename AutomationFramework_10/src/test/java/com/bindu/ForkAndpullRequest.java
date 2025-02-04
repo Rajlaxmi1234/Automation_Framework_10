e05d0422a2694402cd255035fe89e90fdcbbb62d
Rajlaxmi <rajlaxmia23@gmail.com>
2025-02-04 10:58:59 +0530

    ForkAndpullRequest commit

---
+++ AutomationFramework_10/src/test/java/com/bindu/ForkAndpullRequest.java
@@ -0,0 +1,11 @@
+package com.bindu;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ForkAndpullRequest {
+@Test
+public void practice() {
+	Reporter.log("ForkAndpullRequest has been added", true);
+}
+}
