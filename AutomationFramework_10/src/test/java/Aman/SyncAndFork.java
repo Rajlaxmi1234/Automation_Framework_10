9a0ad273c9246d94ed1eebf085b23a1dc2e02886
dell <dell@DESKTOP-M1U6SU2>
2025-02-05 10:23:19 +0530

    SyncAndFork class added

---
+++ AutomationFramework_10/src/test/java/Aman/SyncAndFork.java
@@ -0,0 +1,11 @@
+package Aman;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SyncAndFork {
+@Test
+public void syncAndFork() {
+	Reporter.log("SyncAndFork calss loaded");
+}
+}
